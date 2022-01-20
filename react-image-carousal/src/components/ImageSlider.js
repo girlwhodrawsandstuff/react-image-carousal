46ec037154db7e53642411f3688be14bebfb397b
Varsha Singh <varsha.singh0497@gmail.com>
2022-01-20 15:00:57 +0530

    feat: add functionality to carousal left and right arrows

--- react-image-carousal/src/components/ImageSlider.js
+++ react-image-carousal/src/components/ImageSlider.js
@@ -1,22 +1,43 @@
-import React, {useState} from 'react';
-import { SliderData } from './SliderData';
-import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
+import React, { useState } from "react";
+import { SliderData } from "./SliderData";
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
 const ImageSlider = ({ slides }) => {
-const [current, setCurrent] = useState(0);
-const length = slides.length;
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
 
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
 
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  console.log(current);
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
 
   return (
-      <section className="slider">
-        <FaArrowAltCircleLeft className="left-arrow" />
-        <FaArrowAltCircleRight className="right-arrow" />
-        {SliderData.map((slide, index) => {
-            return <img src={slide.image} alt="3D images"/>
-        })}
-      </section>
-  )
+    <section className="slider">
+      <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide} />
+      <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide} />
+      {SliderData.map((slide, index) => {
+        return (
+          <div
+            className={index === current ? "slide active" : "slide"}
+            key={index}
+          >
+            {index === current && (
+              <img src={slide.image} alt="3D images" className="image" />
+            )}
+          </div>
+        );
+      })}
+    </section>
+  );
 };
 
 export default ImageSlider;
