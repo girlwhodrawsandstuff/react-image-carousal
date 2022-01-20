a4e278d57ae235bd5c20740d42dc29d321f8a013
Varsha Singh <varsha.singh0497@gmail.com>
2022-01-20 14:41:05 +0530

    feat: add image slider icons to page

--- react-image-carousal/src/components/ImageSlider.js
+++ react-image-carousal/src/components/ImageSlider.js
@@ -1,15 +1,21 @@
 import React, {useState} from 'react';
 import { SliderData } from './SliderData';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
-const ImageSlider = () => {
-const [current, setCurrent] = useState(0)
+const ImageSlider = ({ slides }) => {
+const [current, setCurrent] = useState(0);
+const length = slides.length;
+
+
 
   return (
-      <>
+      <section className="slider">
+        <FaArrowAltCircleLeft className="left-arrow" />
+        <FaArrowAltCircleRight className="right-arrow" />
         {SliderData.map((slide, index) => {
-            return <img src={slide.image} alt=""/>
+            return <img src={slide.image} alt="3D images"/>
         })}
-      </>
+      </section>
   )
 };
 
