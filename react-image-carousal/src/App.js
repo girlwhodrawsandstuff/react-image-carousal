b2362f559d34fbb0416fea3631c00bba8d037a07
Varsha Singh <varsha.singh0497@gmail.com>
2022-01-20 19:35:01 +0530

    feat: add rotate animation to slider

--- react-image-carousal/src/App.js
+++ react-image-carousal/src/App.js
@@ -3,7 +3,7 @@ import ImageSlider from './components/ImageSlider';
 import { SliderData } from './components/SliderData';
 
 function App() {
-  return <ImageSlider slides={SliderData} animationType="scale" />
+  return <ImageSlider slides={SliderData} animationType="rotate" />
 }
 
 export default App;
